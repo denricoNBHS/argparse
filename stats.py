f4a72c68d22fd202266801b17f88abc6b95dad61
Omer Ashraf <17503@northberen.k12.nj.us>
2021-01-22 21:42:26 +0000

    adding argparse

--- stats.py
+++ stats.py
@@ -1,11 +1,19 @@
 import argparse
-from statistics import mean, median
-
-""" 
-Write a command line tool that accepts a list of (one or more) 
-numbers and returns their mean by default or, if the '-m' or '--median'
-options are included, their median instead.
-
-The mean and median functions from the statistics module have been
-included for you.
-"""
+def mean(nums: list):
+  return sum(nums)/len(nums)
+def median(nums: list):
+  nums.sort()
+  if len(nums) % 2 == 1:
+     return nums[len(nums) // 2]
+  else:
+    median1 = nums[len(nums) // 2]
+    median2 = nums[(len(nums) // 2) -1]
+    return (median1 + median2)/2
+parser = argparse.ArgumentParser(description='Process some integers to find the mean or median')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer to find mean or median')
+parser.add_argument('-m','--median',dest='accumulate', action='store_const',
+                    const=median, default=mean,
+                    help='find the median (default: find the mean)')
+args = parser.parse_args()
+print(args.accumulate(args.integers))
