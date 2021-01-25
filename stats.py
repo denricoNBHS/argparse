ad7ea912e624a36e2e286257c855aa2bd66e88e2
Vim597 <71149880+Vim597@users.noreply.github.com>
2021-01-25 07:25:41 -0800

    Update stats.py

--- stats.py
+++ stats.py
@@ -9,12 +9,3 @@ options are included, their median instead.
 The mean and median functions from the statistics module have been
 included for you.
 """
-parser = argparse.ArgumentParser(description='Find median or mean of integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--median', dest='accumulate', action='store_const',
-                    const=median, default=mean,
-                    help='sum the integers (default: find the mean)')
-
-args = parser.parse_args()
-print(args.accumulate(args.integers))
