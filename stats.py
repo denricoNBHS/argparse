30a203b19d05bbde54fdeba6b7cef97d51f7ff93
Dave Enrico <denrico@northbergen.k12.nj.us>
2021-01-26 00:14:32 +0000

    Add flags for mean, median, and (pop) stdev

--- stats.py
+++ stats.py
@@ -4,11 +4,16 @@ from statistics import mean, median, stdev
 
 parser = argparse.ArgumentParser(description='Calculates central tendency of a list.')
 parser.add_argument('data', type=float, metavar='N', nargs='+', help='numeric data')
+parser.add_argument('--mean', action='store_true')
 parser.add_argument('--median', action='store_true')
-
+parser.add_argument('--stdev', action='store_true')
 args = parser.parse_args()
 
 print(f"data: {args.data}")
+# Works just as well if you omit == True
+if args.mean == True:
+    print(f"mean: {mean(args.data)}")
 if args.median == True:
-	print(f"median: {median(args.data)}")
-
+    print(f"median: {median(args.data)}")
+if args.stdev == True:
+    print(f"stdev: {stdev(args.data)}")
